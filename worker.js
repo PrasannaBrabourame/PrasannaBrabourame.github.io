d9b71d82e12d04aef03781b05bcd9a31cf5b301e
Prasanna <prasanna18101994@gmail.com>
2019-01-11 19:19:58 +0530

    Full Completed

--- worker.js
+++ worker.js
@@ -1,51 +1,40 @@
 self.onmessage = function (e) {
-    checkPrime = num => {
-        var prime = 1;
-        for (let b = 2; b < num; b++) {
-            if (num % b == 0) {
-                prime = 0;
-                break;
-            }
-        }
-        if (prime)
-            return 1;
-        else
-            return 0;
-    }
     primeArry = max => {
-        const primeArry = new Array(max).fill(true)
-        for (let i = 2; i < Math.sqrt(max); i++) {
-            if (primeArry[i]) {
-                for (let j = Math.pow(i, 2); j < max; j += i) {
-                    primeArry[j] = false
+        const notPrime = new Uint8Array(max);
+        const primes = [];
+        notPrime[0] = notPrime[1] = 1;
+        for (let i = 2; i < max; i++) {
+            if (notPrime[i] === 0) {
+                primes.push(i);
+                for (let j = 2 * i; j < max; j += i) {
+                    notPrime[j] = 1;
                 }
             }
         }
-        return primeArry.reduce((primes, isPrime, i) => {
-            if (isPrime && i > 1) {
-                primes.push(i)
-            }
-
-            return primes
-        }, [])
+        return {primes,notPrime}
     }
     if (e.data.type === "sumOfPrimes") {
         const max = primeArry(Number(e.data.value));
-        const sumvalue = max.reduce((a, b) => a + b);
+        const sumvalue = max.primes.reduce((a, b) => a + b);
         self.postMessage({ value: sumvalue, type: e.data.type })
     } else if (e.data.type === "sumOfConsecutives") {
-        var i = 1;
-        var sum = 2;
-        var max_sum = 0;
-        while (sum < Number(e.data.value)) {
-            if (checkPrime(i))
-                sum += i;
-            if (checkPrime(sum)) {
-                if (sum > max_sum)
-                    max_sum = sum;
+        const max = primeArry(Number(e.data.value));
+        let maxSum = 0;
+        let maxRun = -1;
+        for (let i = 0; i < max.primes.length; i++) {
+
+            let sum = 0;
+            for (let j = i; j < max.primes.length; j++) {
+                sum += max.primes[j];
+                if (sum > e.data.value)
+                    break;
+                if (!max.notPrime[sum] && sum > maxSum && j - i > maxRun) {
+                    maxRun = j - i;
+                    maxSum = sum;
+                }
             }
-            i++;
         }
-        self.postMessage({ value: max_sum, type: e.data.type })
+
+        self.postMessage({ value: maxSum, type: e.data.type })
     }
 };
